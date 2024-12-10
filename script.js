114c715e5156ce90706a7612d7c870ce6637d2ec
ZriouilAnas <120396174+ZriouilAnas@users.noreply.github.com>
2024-12-10 15:47:18 +0100

    teest

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById("btn");
 const h1 = document.querySelector("h1");
-btn.addEventListener("click", () => {
+
+btn.addEventListener("click", tglRED);
+
+const tglRED = () => {
   h1.classList.toggle("red");
-});
+};
